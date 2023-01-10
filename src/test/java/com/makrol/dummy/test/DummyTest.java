5d6408d784065f8b62232be891dd8c43aaa46345
mkrol <krol.mariya@gmail.com>
2023-01-10 15:45:41 +0300

    Initial commit

---
+++ src/test/java/com/makrol/dummy/test/DummyTest.java
@@ -0,0 +1,12 @@
+package com.makrol.dummy.test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+
+    @Test
+    public void shouldBeSuccessful() {
+        Assertions.assertEquals(4, 2 + 2);
+    }
+}
